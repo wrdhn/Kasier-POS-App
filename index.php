c156ffed36012e0c23a7c894a7c84c6689ae5fc4
wrdhn <yhustyawardhana09@gmail.com>
2025-04-20 14:31:45 +0700

    Add initial project files including index.php and Bootstrap CSS

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+    include "./src/config/connection.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kasier-App</title>
+</head>
+<body>
+    <header>
+        <?php 
+            include "./src/includes/header.php";
+        ?>
+    </header>
+    <main>
+        <?php
+        if (isset($_GET['page'])) {
+            $page = $_GET['page'];
+            if ($page === "home") {
+                include "./src/pages/home.php";
+            } else if ($page === "pelanggan") {
+                include "./src/pages/pelanggan/index.php";
+            } else if ($page === "penjualan") {
+                include "./src/pages/penjualan/index.php";
+            } else if ($page === "produk") {
+                include "./src/pages/produk/index.php";
+            } else if ($page === "detail_penjualan") {
+                include "./src/pages/detail-penjualan/index.php";
+            } else {
+                include "./src/pages/404.php"; // 404 page
+            }
+        }
+        ?>
+    </main>
+</body>
+</html>
